b62fb3e17b871472183cb0ab544500f688d7470d
skynid <72501937+skynid@users.noreply.github.com>
2021-03-18 18:23:00 +0530

    Add files via upload

---
+++ findascii.java
@@ -0,0 +1,13 @@
+
+public class findascii {
+	public static void main(String args[])
+	{
+		char ch = 'a';
+        int ascii = ch;
+        
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+	}
+	
+
+}
