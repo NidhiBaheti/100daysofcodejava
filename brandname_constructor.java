800e7c586bd69af416b4d1353209a46a7aba12dd
skynid <72501937+skynid@users.noreply.github.com>
2021-03-26 13:39:07 +0530

    Add files via upload

---
+++ brandname_constructor.java
@@ -0,0 +1,23 @@
+
+class brandname_constructor {
+String brand;
+	
+	//your constructor here
+	
+	public String getBrand() {
+		return brand;
+	}
+
+	void run() {
+		System.out.println("Car is running...");
+	}
+	
+}
+ class Sample {
+	
+	public static void main(String[] args) {
+
+				brandname_constructor ford = new  brandname_constructor();			
+	}
+}
+
