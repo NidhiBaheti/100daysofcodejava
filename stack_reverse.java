86667a42c4cc6413ea7ee5a988b4af423f017e78
skynid <72501937+skynid@users.noreply.github.com>
2021-04-03 19:07:37 +0530

    Add files via upload

---
+++ stack_reverse.java
@@ -0,0 +1,53 @@
+import java.util.*;
+public class stack_reverse {
+	 public static void insertAtBottom(Stack<Integer> s, int item)
+	    {
+	        // base case: if the stack is empty, insert the given item at the bottom
+	        if (s.empty())
+	        {
+	            s.push(item);
+	            return;
+	        }
+	 
+	        // Pop all items from the stack and hold them in the call stack
+	        int top = s.pop();
+	        insertAtBottom(s, item);
+	 
+	        // After the recursion unfolds, push each item in the call stack
+	        // at the top of the stack
+	        s.push(top);
+	    }
+	 
+	    // Recursive function to reverse a given stack
+	    public static void reverseStack(Stack<Integer> s)
+	    {
+	        // base case: stack is empty
+	        if (s.empty()) {
+	            return;
+	        }
+	 
+	        // Pop all items from the stack and hold them in the call stack
+	        int item = s.pop();
+	        reverseStack(s);
+	 
+	        // After the recursion unfolds, insert each item in the call stack
+	        // at the bottom of the stack
+	        insertAtBottom(s, item);
+	    }
+	 
+	    public static void main(String[] args)
+	    {
+	        Stack<Integer> s = new Stack<>();
+	        for (int i = 1; i <= 5; i++) {
+	            s.push(i);
+	        }
+	 
+	        reverseStack(s);
+	 
+	        System.out.print("Reversed stack is ");
+	        while (!s.empty()) {
+	            System.out.print(s.pop() + " ");
+	        }
+	    }
+}
+
---
+++ stacks_minimumelement.java
@@ -0,0 +1,116 @@
+import java.util.*;
+public class stacks_minimumelement {
+
+	private Stack<Integer> s;       // main stack to store elements
+    private Stack<Integer> aux;     // auxiliary stack to store minimum elements
+ 
+    // Constructor
+     public stacks_minimumelement()
+    {
+        s = new Stack<>();
+        aux = new Stack<>();
+    }
+ 
+    // Inserts a given element on top of the stack
+    public void push(int x)
+    {
+        // push the given element into the main stack
+        s.push(x);
+ 
+        // if the auxiliary stack is empty, push the given element into it
+        if (aux.empty()) {
+            aux.push(x);
+        }
+        else {
+            // push the given element into the auxiliary stack
+            // if it is less than or equal to the current minimum
+            if (aux.peek() >= x) {
+                aux.push(x);
+            }
+        }
+    }
+ 
+    // Removes the top element from the stack and returns it
+    public int pop()
+    {
+        if (empty())
+        {
+            System.out.println("Stack underflow!!");
+            return -1;
+        }
+ 
+        // remove the top element from the main stack
+        int top = s.pop();
+ 
+        // remove the top element from the auxiliary stack
+        // only if it is minimum
+        if (top == aux.peek()) {
+            aux.pop();
+        }
+ 
+        // return the removed element
+        return top;
+    }
+ 
+    // Returns the top element of the stack
+    public int peek() {
+        return s.peek();
+    }
+ 
+    // Returns the total number of elements in the stack
+    public int size() {
+        return s.size();
+    }
+ 
+    // Returns the true if the stack is empty; false otherwise
+    public boolean empty() {
+        return s.empty();
+    }
+ 
+    // Returns the minimum element from the stack in constant time
+    public int min()
+    {
+        if (aux.empty())
+        {
+            System.out.print("Stack underflow!! ");
+            return -1;
+        }
+ 
+        return aux.peek();
+    }
+}
+ 
+class Main
+{
+    public static void main (String[] args)
+    {
+    	stacks_minimumelement s = new stacks_minimumelement();
+ 
+        s.push(6);
+        System.out.println(s.min());    // prints 6
+ 
+        s.push(7);
+        System.out.println(s.min());    // prints 6
+ 
+        s.push(8);
+        System.out.println(s.min());    // prints 6
+ 
+        s.push(5);
+        System.out.println(s.min());    // prints 5
+ 
+        s.push(3);
+        System.out.println(s.min());    // prints 3
+ 
+        s.pop();
+        System.out.println(s.min());    // prints 5
+ 
+        s.push(10);
+        System.out.println(s.min());    // prints 5
+ 
+        s.pop();
+        System.out.println(s.min());    // prints 5
+ 
+        s.pop();
+        System.out.println(s.min());    // prints 6
+    }
+}
