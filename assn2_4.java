2907dd54e3c55434ca7c613f39db877377161439
skynid <72501937+skynid@users.noreply.github.com>
2021-05-22 19:17:00 +0530

    Add files via upload

---
+++ assn2_4.java
@@ -0,0 +1,16 @@
+/*Write a java program for Pyramid Example */
+package assn2;
+
+public class assn2_4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<5;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+	}
+}
